33bc16212e404315317cfb61e18fe4935594736d
Pavl0ffN <pavl0ffn94@gmail.com>
2023-04-28 17:11:45 +0400

    animation

--- main.js
+++ main.js
@@ -10,51 +10,46 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 //Speedometer
+
+const circle = document.querySelector('.progress-ring__circle');
+const radius = circle.r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = {circumference};
+
+function setProgress(percent) {
+  const offset = circumference - (percent / 100) * circumference;
+  circle.style.strokeDashoffset = offset;
+}
+
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
+const arrow = document.querySelector('.speed-arrow ');
+console.log(arrow);
 
-const imgSpeed = document.querySelector('svg');
-imgSpeed.style.fill = 'red';
+const color1 = '#7A0BC0';
+const color2 = '#0B8AC0';
+const color3 = '#fa58b6';
 
 btn1.addEventListener('click', () => {
-  console.log('btn1');
-  imgSpeed.style.fill = 'red';
-  console.dir(imgSpeed);
+  arrow.classList.add('lvl-1');
+  arrow.classList.remove('lvl-2', 'lvl-3');
+  circle.style.stroke = color1;
+  setProgress(33);
 });
 
-function animateFill(color) {
-  const circle = document.getElementById('circle');
-  const currentFill = circle.getAttribute('fill');
-  let fillPercentage = 0;
-  let targetFill = '';
+btn2.addEventListener('click', () => {
+  arrow.classList.add('lvl-2');
+  arrow.classList.remove('lvl-1', 'lvl-3');
+  circle.style.stroke = color2;
+  setProgress(66);
+});
 
-  switch (color) {
-    case 'lvl1':
-      fillPercentage = 33;
-      targetFill = '#7A0BC0';
-      break;
-    case 'lvl2':
-      fillPercentage = 66;
-      targetFill = '#0B8AC0';
-      break;
-    case 'lvl3':
-      fillPercentage = 99;
-      targetFill = '#E105E5';
-      break;
-  }
-
-  if (currentFill === 'none') {
-    circle.setAttribute('fill', targetFill);
-  } else {
-    const currentPercentage = parseInt(currentFill);
-    const interval = setInterval(function () {
-      if (currentPercentage < fillPercentage) {
-        currentPercentage++;
-        circle.setAttribute('fill', `${targetFill} ${currentPercentage}%`);
-      } else {
-        clearInterval(interval);
-      }
-    }, 10);
-  }
-}
+btn3.addEventListener('click', () => {
+  arrow.classList.add('lvl-3');
+  arrow.classList.remove('lvl-2', 'lvl-1');
+  circle.style.stroke = color3;
+  setProgress(98);
+});
+setProgress(33);
